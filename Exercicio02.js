7d3a0884a1955cf926929a066382d0d8fa8b76a3
Leticia Lind <48027331862@imaccommand011.local>
2025-11-19 06:56:38 -0800

    Exercicio01

--- Exercicio02.js
+++ Exercicio02.js
@@ -1,28 +1,23 @@
-let numeros = prompt("Digite um número de 1 a 7: ")
-
-switch (numeros) {
-    case "1":
-        console.log("Segunda-feira")
-        break;
-case "2":
-        console.log("Terça-feira")
-        break;
-case "3":
-        console.log("Quarta-feira")
-        break;
-case "4":
-        console.log("Quinta-feira")
-        break;
-case "5":
-        console.log("Sexta-feira")
-        break;
-case "6":
-        console.log("Sábado")
-        break;
-case "7":
-        console.log("Domingo")
-        break;
-
-    default:
-        break;
+let nome = (prompt("Digite seu email"))
+if ( nome == "admin") {
+    console.log("Acesso Liberado")
 }
+
+
+else{
+   console.log("Acesso Negado")
+}
+
+
+
+// ------------------ senha --------------------
+
+
+let senha = (prompt("Digite sua senha"))
+  if (senha == "senha123") {
+     console.log ("Acesso Liberado")
+  }
+
+  else{
+    console.log("Acesso Negado")
+  }

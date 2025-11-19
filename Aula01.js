fbf4111837f6d955870309170a2e370b175c3ce1
Leticia Lind <48027331862@imaccommand011.local>
2025-11-19 05:39:38 -0800

    Meu primeiro programa em js

---
+++ Aula01.js
@@ -0,0 +1,61 @@
+// window.alert("Minha primeira menssagem!")
+
+// window.confirm("Hoje é sexta?")
+
+//comentario em linha
+
+/**
+ * comentario em varias linhas
+ */
+
+
+
+// let nome = window.prompt("Qual é o seu nome?")
+
+// window.alert("Bem vindo(a)!" + nome)
+
+// console.log("Apresenta mensagem na tela")
+
+// let numero01 = parseInt(prompt("Digite o primeiro número: "))
+
+
+// let numero02 = parseInt(prompt("Digite o segundo número: "))
+
+
+// let soma = numero01 + numero02
+
+// console.log(soma)
+
+// let num01 = parseFloat(prompt("Digite algum número: "))
+
+// let num02 = parseFloat(prompt("Digete algum número: "))
+
+// let resultado = num01 + num02
+
+// console.log(resultado)
+
+// 
+
+let estacoes = prompt("Digite uma esação do ano: ")
+
+switch (estacoes) {
+    case "Verão":
+        console.log("Muito calor e praia")
+        break;
+
+
+    case "Outono":
+        console.log("As folhas caem no final")
+        break;
+
+    case "Inverno":
+        console.assertlog("Está muito frio")
+        break;
+
+    case "Primavera":
+        console.log("Eu vejo flores em você...")
+        break;
+    default:
+        console.log("Não existe essa estação!!!")
+        break;
+}

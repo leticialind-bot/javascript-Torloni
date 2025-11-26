ce5ecd4a76713351b6bf58c3b15f1dace7cd0a13
Leticia Lind <48027331862@imaccommand011.local>
2025-11-26 11:49:53 -0800

    Iniciado os exercicios sobre o laco for

---
+++ estrutura de repetição/exercicio02.js
@@ -0,0 +1,16 @@
+// 2. Imprimir nú pares de 1 a 10.
+
+for(let i = 1; i <= 10; i++){
+    if(i % 2 == 0){
+        //par
+        console.log(i);
+
+
+    }
+
+}
+
+
+for(let i = 2; i<= 10; i= i=+2){
+    console.log(i);
+}
---
+++ estrutura de repetição/for.js
@@ -0,0 +1,21 @@
+// sintaxe = escrita do codigo 
+
+//for - laço de repetição = loop
+// um laço de repetição tem voltas 
+
+//laço contado - quando sabemos quantas voltas 
+//laço condicional - irá fazer as voltas se a condição for verdadeira
+
+//for([inicialização]; [condição]; [incremento];){
+// }
+
+// a variavel i vale 0 ou seja essa variavel armazena as voltas 
+// a condicao verifica o numero que o loop esta 
+for(let i = 0; i <= 9; i = i + 1){
+    console.log("A volta que for está é: " + i);
+}
+
+//utilize o for para exibir do 1 ao 10
+for(let i = 1; i <= 10; i = i + 1){
+    console.log(1)
+}
